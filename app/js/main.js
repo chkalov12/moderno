aa5db51841ebef9e3599605a969e89d50dc087a5
chkalov12 <chkalov007@gmail.com>
2020-04-08 22:35:22 +0300

    add product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,24 @@
 $(function(){
 
+ 
+    $(".rate-star").rateYo({
+      rating: 5,
+      starWidth: "12px",
+      readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+       
+
+
+
     let mixer = mixitup('.products__inner-box');
+
 
 
 });
