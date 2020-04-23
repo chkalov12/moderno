72936b8475599806149ac92b6643f735d6e93efa
chkalov12 <chkalov007@gmail.com>
2020-04-23 15:14:27 +0300

    adaptive complete

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,34 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1901,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 1401,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 801,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
